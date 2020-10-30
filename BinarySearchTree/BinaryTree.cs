bcc2a4b0e8003e6a2cab4085127be2a34c5cae93
Christopher Couch <c.alan.couch@gmail.com>
2020-10-30 11:30:45 -0500

    Init. Created classes for Node and BinaryTree. Made member variables, constructors, and began logic for Add() with BinaryTree class.

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        public Node rootNode;
+
+        public BinaryTree()
+        {
+            rootNode = null;
+        }
+
+        public void Add(Node newNode)
+        {
+            if(rootNode == null)
+            {
+                rootNode = newNode;
+            }
+            else if(newNode.Value > rootNode.Value)
+            {
+                rootNode.rightNode = newNode;
+            }
+            else if(newNode.Value <= rootNode.Value)
+            {
+                rootNode.leftNode = newNode;
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int Value;
+        public Node leftNode;
+        public Node rightNode;
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
